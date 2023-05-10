ac313db4d2701ff819563f9577256379b3a5c628
Vikas-Bawa <vikasbawa55@gmail.com>
2023-05-10 18:16:44 +0530

    wedge panels

--- src/Constants.js
+++ src/Constants.js
@@ -15,17 +15,17 @@ import commonWedge9 from './assets/wedges/fortune9.png';
 
 
 export const data = [
-    { "label": "Dell LAPTOP", "value": 1, "image": commonWedge1 }, // padding
-    { "label": "IMAC PRO", "value": 2, "image": commonWedge2 }, //font-family
+    { "label": "LAPTOP", "value": 1, "image": commonWedge1 }, // padding
+    { "label": "IMAC", "value": 2, "image": commonWedge2 }, //font-family
     { "label": "SUZUKI", "value": 3, "image": commonWedge3 }, //color
     { "label": "HONDA", "value": 4, "image": commonWedge4 }, //font-weight
     { "label": "FERRARI", "value": 5, "image": commonWedge5 }, //font-size
     { "label": "APARTMENT", "value": 6, "image": commonWedge6 }, //background-color
     { "label": "IPAD PRO", "value": 7, "image": commonWedge7 }, //nesting
-    { "label": "LAND ROVER", "value": 8, "image": commonWedge8 }, //bottom
-    { "label": "MOTOROLLA", "value": 9, "image": commonWedge9 }, //sans-serif
+    { "label": "ANFIG", "value": 8, "image": commonWedge8 }, //bottom
+    { "label": "MOTO", "value": 9, "image": commonWedge9 }, //sans-serif
     { "label": "BMW", "value": 10, "image": commonWedge10 },
-    { "label": "LAMBORGHINI", "value": 11, "image": commonWedge11 },
+    { "label": "LAMBO", "value": 11, "image": commonWedge11 },
     { "label": "MCU", "value": 12, "image": commonWedge12 },
     { "label": "HOVER", "value": 13, "image": commonWedge13 },
     { "label": "CRUISER", "value": 14, "image": commonWedge14 },
--- src/wheel/index.jsx
+++ src/wheel/index.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import { data } from '../Constants';
-import NFT from "../assets/NFTs/SMB Mythic 28 .png";
 import applause from "../assets/applause.mp3";
 import centrePanel from "../assets/centrePanel.png";
 import frame from "../assets/frame.png";
@@ -13,9 +12,9 @@ import wheel from "../assets/wheel.mp3";
 import './style.css';
 function Wheel() {
     const onMountRef = useRef(true);
-    var padding = { top: 20, right: 40, bottom: 20, left: 40 },
-        w = 500 - padding.left - padding.right,
-        h = 500 - padding.top - padding.bottom,
+    var padding = { top: 10, right: 50, bottom: 10, left: 50 },
+        w = 520 - padding.left - padding.right,
+        h = 520 - padding.top - padding.bottom,
         r = Math.min(w, h) / 2,
         rotation = 0,
         oldrotation = 0,
@@ -46,14 +45,14 @@ function Wheel() {
             .attr("height", h + padding.top + padding.bottom);
         svg.append('svg:image')
             .attr('xlink:href', frame)
-            .attr("width", '497')
-            .attr("height", '499')
+            .attr("width", '515')
+            .attr("height", '515')
             .attr("x", 0)
             .attr("y", 0)
 
         container = svg.append("g")
             .attr("class", "chartholder")
-            .attr("transform", "translate(" + (w / 2 + padding.left - 1) + "," + (h / 2 + padding.top + 13) + ")");
+            .attr("transform", "translate(" + (w / 2 + padding.left - 2) + "," + (h / 2 + padding.top + 12) + ")");
         vis = container.append("g");
 
         var pie = d3.layout.pie().sort(null).value(function (d) { return 1; });
@@ -72,20 +71,20 @@ function Wheel() {
             // .attr("fill", function (d, i) {
             //     return "url(#wedgeImg" + (i + 1) + ")";
             // })
-            .attr("d", function (d) { console.log(arc(d)); return arc(d); })
-            .on("mouseover", function (d) {
-                d3.select(this.parentNode).transition()
-                    .duration('500')
-                    .attr("transform", function (d) {
-                        return "scale(1.1)";
-                    })
-            }).on("mouseout", function (d) {
-                d3.select(this.parentNode).transition()
-                    .duration('500')
-                    .attr("transform", function (d) {
-                        return "scale(1)";
-                    })
-            })
+            .attr("d", function (d) { return arc(d); })
+        // .on("mouseover", function (d) {
+        //     d3.select(this.parentNode).transition()
+        //         .duration('500')
+        //         .attr("transform", function (d) {
+        //             return "scale(1.1)";
+        //         })
+        // }).on("mouseout", function (d) {
+        //     d3.select(this.parentNode).transition()
+        //         .duration('500')
+        //         .attr("transform", function (d) {
+        //             return "scale(1)";
+        //         })
+        // })
 
         var arc2 = d3.svg.arc()
             .outerRadius(r)
@@ -97,51 +96,51 @@ function Wheel() {
         //     .attr("class", "slice");
         arcs.append("path")
             .attr("fill", 'rgba(0,0,0,0.1)')
-            .attr("d", function (d) { console.log(arc2(d)); return arc2(d); })
+            .attr("d", function (d) { return arc2(d); })
 
-            .attr('stroke', 'black')
-            .attr('stroke-width', '0.5')
-            .on("mouseover", function (d) {
-                d3.select(this.parentNode).transition()
-                    .duration('500')
-                    .attr("transform", function (d) {
-                        return "scale(1.1)";
-                    })
-            }).on("mouseout", function (d) {
-                d3.select(this.parentNode).transition()
-                    .duration('500')
-                    .attr("transform", function (d) {
-                        return "scale(1)";
-                    })
-            })
+        // .attr('stroke', 'black')
+        // .attr('stroke-width', '0.5')
+
+        var arc3 = d3.svg.arc()
+            .outerRadius(170)
+            .innerRadius(170);
+        arcs.append("path")
+            .attr("fill", 'rgba(0,0,0,1)')
+            .attr("d", function (d) { return arc3(d); })
+            .attr("stroke", 'rgba(0,0,0,0.3)')
+            .attr("stroke-width", '0.5')
             .attr('stroke-dasharray', '2 2')
+
+        // .attr('stroke', 'grey')
+
+
         arcs.append('svg:image')
             .attr('xlink:href', function (d, i) {
                 return leftLine;
             })
             .attr("width", 8)
-            .attr("height", 200)
-            .attr("x", -2)
+            .attr("height", 210)
+            .attr("x", -2.5)
             .attr("y", 0)
             .attr("transform", function (d) {
                 d.innerRadius = 0;
                 d.outerRadius = r;
                 d.angle = (d.startAngle + d.endAngle) / 2;
-                return "rotate(" + ((d.startAngle * 180) / Math.PI) + ")translate(" + (0) + ', ' + (-d.outerRadius) + ")";
+                return "rotate(" + ((d.startAngle * 180) / Math.PI) + ")translate(" + (0) + ', ' + (-d.outerRadius - 8) + ")";
             })
         arcs.append('svg:image')
             .attr('xlink:href', function (d, i) {
                 return rightLine;
             })
             .attr("width", 8)
-            .attr("height", 200)
+            .attr("height", 210)
             .attr("x", -6)
             .attr("y", 0)
             .attr("transform", function (d) {
                 d.innerRadius = 0;
                 d.outerRadius = r;
                 d.angle = (d.startAngle + d.endAngle) / 2;
-                return "rotate(" + ((d.endAngle * 180) / Math.PI) + ")translate(" + (0) + ', ' + (-d.outerRadius) + ")";
+                return "rotate(" + ((d.endAngle * 180) / Math.PI) + ")translate(" + (0) + ', ' + (-d.outerRadius - 8) + ")";
             });
         // square image
         arcs.append('svg:image')
@@ -158,50 +157,6 @@ function Wheel() {
                 d.angle = (d.startAngle + d.endAngle) / 2;
                 return "rotate(" + ((d.angle * 180) / Math.PI) + ")translate(" + (0) + ', ' + (-d.outerRadius + 35) + ")";
             })
-            .on("mouseover", function (d) {
-                d3.select(this.parentNode).transition()
-                    .duration('500')
-                    .attr("transform", function (d) {
-                        return "scale(1.1)";
-                    })
-            }).on("mouseout", function (d) {
-                d3.select(this.parentNode).transition()
-                    .duration('500')
-                    .attr("transform", function (d) {
-                        return "scale(1)";
-                    })
-            });
-
-        // NFT image
-        arcs.append('svg:image')
-            .attr('xlink:href', function (d, i) {
-                return NFT;
-            })
-            .attr("width", 30)
-            .attr("height", 30)
-            .attr("x", -15)
-            .attr("y", 0)
-            .attr("transform", function (d) {
-                d.innerRadius = 0;
-                d.outerRadius = r;
-                d.angle = (d.startAngle + d.endAngle) / 2;
-                return "rotate(" + ((d.angle * 180) / Math.PI) + ")translate(" + (0) + ', ' + (-d.outerRadius + 50) + ")";
-            })
-            .attr("style", "opacity:0.5;")
-            .on("mouseover", function (d) {
-                d3.select(this.parentNode).transition()
-                    .duration('500')
-                    .attr("transform", function (d) {
-                        return "scale(1.1)";
-                    })
-            }).on("mouseout", function (d) {
-                d3.select(this.parentNode).transition()
-                    .duration('500')
-                    .attr("transform", function (d) {
-                        return "scale(1)";
-                    })
-            })
-        // .attr("transform-origin", "top left")
         // .on("mouseover", function (d) {
         //     d3.select(this.parentNode).transition()
         //         .duration('500')
@@ -215,6 +170,25 @@ function Wheel() {
         //             return "scale(1)";
         //         })
         // });
+
+        // NFT image
+        // arcs.append('svg:image')
+        //     .attr('xlink:href', function (d, i) {
+        //         return NFT;
+        //     })
+        //     .attr("width", 30)
+        //     .attr("height", 30)
+        //     .attr("x", -15)
+        //     .attr("y", 0)
+        //     .attr("transform", function (d) {
+        //         d.innerRadius = 0;
+        //         d.outerRadius = r;
+        //         d.angle = (d.startAngle + d.endAngle) / 2;
+        //         return "rotate(" + ((d.angle * 180) / Math.PI) + ")translate(" + (0) + ', ' + (-d.outerRadius + 50) + ")";
+        //     })
+        //     .attr("style", "opacity:0.5;")
+
+
         arcs.append("defs")
             .append("radialGradient")
             .attr('id', function (d, i) { return 'grad' + (i + 1) })
@@ -230,14 +204,25 @@ function Wheel() {
                 d.outerRadius = r;
                 d.angle = (d.startAngle + d.endAngle) / 2;
                 console.log('d.startAngle: ' + d.startAngle + 'd.endAngle: ' + d.endAngle + 'dangle : ', (d.angle * 180 / Math.PI));
-                return "rotate(" + (d.angle * 180 / Math.PI) + ")translate(" + (0) + ', ' + (-(d.outerRadius - 20)) + ")";
+                return "rotate(" + (d.angle * 180 / Math.PI) + ")translate(" + (0) + ', ' + (-(d.outerRadius - 15)) + ")";
             })
             .attr("text-anchor", "middle")
             .text(function (d, i) {
                 return data[i].label;
             })
-            .style({ "font-size": "0.5em", "font-weight": "700", 'fill': 'white' })
-
+            .style({ "font-size": "0.7em", "font-weight": "700", 'fill': 'white' })
+        arcs.append("text")
+            .attr("transform", function (d) {
+                d.innerRadius = 0;
+                d.outerRadius = r;
+                d.angle = (d.startAngle + d.endAngle) / 2;
+                return "rotate(" + (d.angle * 180 / Math.PI) + ")translate(" + (0) + ', ' + (-(d.outerRadius - 30)) + ")";
+            })
+            .attr("text-anchor", "middle")
+            .text(function (d, i) {
+                return data[i].value;
+            })
+            .style({ "font-size": "0.7em", "font-weight": "700", 'fill': 'rgba(0,0,0,0.5)' })
 
         //make pointer
         svg.append('svg:image')
